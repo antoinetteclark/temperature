d4d3014db53e2708d7431d47afc777cc60b28e06
antoinette clark <antoinetteclark8865@gmail.com>
2025-03-29 17:52:23 -0400

    variable

---
+++ variables.js
@@ -0,0 +1,23 @@
+console. log("Variables and constants");
+
+let myName= "Samantha";
+let lastName = "Jimenez";
+//numbers
+var myage = 99;
+
+
+
+
+
+
+
+
+
+
+//operations
+
+let num1=10;
+let num2=20;
+
+let addition = num1 + num2;
+let su
